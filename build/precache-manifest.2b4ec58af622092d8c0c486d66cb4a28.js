7763905d2ce78bc69f7046ebba659719ca1bc216
Eugene Lysov <evg.lysov@gmail.com>
2021-01-28 15:27:23 +0200

    1

---
+++ build/precache-manifest.2b4ec58af622092d8c0c486d66cb4a28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd37664914a0723ccf6120edc20663a5",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "0c388fbe09464f03eea9",
+    "url": "/test1/static/css/main.ae5af742.chunk.css"
+  },
+  {
+    "revision": "3f4fcf8596be80f7ced8",
+    "url": "/test1/static/js/2.e43e464b.chunk.js"
+  },
+  {
+    "revision": "0c388fbe09464f03eea9",
+    "url": "/test1/static/js/main.e77948aa.chunk.js"
+  },
+  {
+    "revision": "79f5e148a66c1558bd04",
+    "url": "/test1/static/js/runtime-main.bbc50164.js"
+  }
+]);
