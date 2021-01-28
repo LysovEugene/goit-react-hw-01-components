f7da6ae28c0805cf78e43edb64d7eed7a5c8b823
Eugene Lysov <evg.lysov@gmail.com>
2021-01-28 15:03:20 +0200

    Updates

---
+++ precache-manifest.dcb720315f16703daaff4a0b2531f175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb95821f8046d667857a4ceec173819b",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "3da56a61df1c16f8572d",
+    "url": "/test1/static/css/main.06c4c370.chunk.css"
+  },
+  {
+    "revision": "3f4fcf8596be80f7ced8",
+    "url": "/test1/static/js/2.e43e464b.chunk.js"
+  },
+  {
+    "revision": "3da56a61df1c16f8572d",
+    "url": "/test1/static/js/main.617c948c.chunk.js"
+  },
+  {
+    "revision": "79f5e148a66c1558bd04",
+    "url": "/test1/static/js/runtime-main.bbc50164.js"
+  }
+]);
