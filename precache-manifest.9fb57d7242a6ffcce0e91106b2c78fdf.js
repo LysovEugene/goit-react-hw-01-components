228e999b8663c2f800ab773e755a74bedc67d97d
Eugene Lysov <evg.lysov@gmail.com>
2021-01-28 15:29:37 +0200

    Updates

---
+++ precache-manifest.9fb57d7242a6ffcce0e91106b2c78fdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6503764093e177c42b8766872dcf98f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "0c388fbe09464f03eea9",
+    "url": "/goit-react-hw-01-components/static/css/main.ae5af742.chunk.css"
+  },
+  {
+    "revision": "3f4fcf8596be80f7ced8",
+    "url": "/goit-react-hw-01-components/static/js/2.e43e464b.chunk.js"
+  },
+  {
+    "revision": "0c388fbe09464f03eea9",
+    "url": "/goit-react-hw-01-components/static/js/main.e77948aa.chunk.js"
+  },
+  {
+    "revision": "1deac508ae55ff559187",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.a749bb14.js"
+  }
+]);
